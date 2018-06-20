8657d5ecd737e6163278d418ef4f658a5569187b
Adam Horsman <adam@sakari.io>
2018-06-19 17:20:39 -0700

    updating links

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name='requestbin',
     version='2.0.0',
     author='Sakari',
-    author_email='apitest@sakari.io',
+    author_email='chris@sakari.io',
     description='HTTP request collector and inspector',
     packages=find_packages(),
     install_requires=['feedparser'],
